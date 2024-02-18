76d07762d592d8cc6702e2dc290ecd73f900c10c
jerrita <je5r1ta@icloud.com>
2024-02-18 14:22:49 +0800

    add: plugins for week,zhihu upd: plugins.bang wait: anon.storage

--- plugins/example/zhihu.py
+++ plugins/example/zhihu.py
@@ -1,10 +1,32 @@
-import asyncio
 import aiohttp
 from datetime import datetime
 from pytz import timezone
 from anon import Bot, PluginManager, Plugin
 from anon.logger import logger
 from bs4 import BeautifulSoup
+
+
+def parse_hot_list(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    hot_list = []
+    ol = soup.find('ol')  # 找到包含热门话题的ol标签
+    for li in ol.find_all('li')[:15]:  # 只获取前15个li标签
+        a_tag = li.find('a')
+        question_url = a_tag['href']
+        question_title = a_tag.get_text()
+        hot_list.append((question_title, question_url))
+    return hot_list
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def get_zhihu_hot_list():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, 'https://github.com/justjavac/zhihu-trending-hot-questions')
+        return parse_hot_list(html)
 
 
 class CronPlugin(Plugin):
@@ -25,32 +47,12 @@ class CronPlugin(Plugin):
             logger.info('Cron triggered on the xth minute of specified hours')
 
             # 获取知乎热榜并发送消息
-            hot_list = await self.get_zhihu_hot_list()
+            hot_list = await get_zhihu_hot_list()
             message = "知乎热榜:\n" + "\n".join(
                 [f"{index}: {title}\n链接: {url}" for index, (title, url) in enumerate(hot_list, 1)])
             await self.bot.send_private_message(114514, message)
         else:
             logger.info('Cron triggered but not at the xth minute of specified hours')
-
-    async def get_zhihu_hot_list(self):
-        async with aiohttp.ClientSession() as session:
-            html = await self.fetch(session, 'https://github.com/justjavac/zhihu-trending-hot-questions')
-            return await self.parse_hot_list(html)
-
-    async def fetch(self, session, url):
-        async with session.get(url) as response:
-            return await response.text()
-
-    async def parse_hot_list(self, html):
-        soup = BeautifulSoup(html, 'html.parser')
-        hot_list = []
-        ol = soup.find('ol')  # 找到包含热门话题的ol标签
-        for li in ol.find_all('li')[:15]:  # 只获取前15个li标签
-            a_tag = li.find('a')
-            question_url = a_tag['href']
-            question_title = a_tag.get_text()
-            hot_list.append((question_title, question_url))
-        return hot_list
 
 
 # 注册插件
